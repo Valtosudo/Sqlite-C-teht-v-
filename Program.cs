c7406724a43aa35cf5355bad0d013f21b49e9f39
Valtosudo <valtsu.vaajalahti@gmail.com>
2025-09-09 15:44:10 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Hei! Haluatko lisätä henkilön (H) vai lemmikin (L)?");
+
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            string vastaus = input.ToUpper();
+
+            if (vastaus == "H")
+            {
+                break;
+            }
+            else if (vastaus == "L")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Väärä syöte. Yritä uudelleen.");
+            }
+        }
     }
 }
