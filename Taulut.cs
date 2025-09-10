c89eb80df13e1f2f3836279912bdba952488fdde
Valtosudo <valtsu.vaajalahti@gmail.com>
2025-09-10 13:30:29 +0300

    joo

--- Taulut.cs
+++ Taulut.cs
@@ -4,29 +4,33 @@ using Microsoft.Data.Sqlite;
 
 public class Taulut
 {
-    private string _connectionString = "Data Source = HenkilotJaLemmikit.db";
+    private string _connectionString = "Data Source = Lemmikki.db";
 
     public Taulut()
     {
-        var connection = new SqliteConnection(_connectionString);
-        connection.Open();
-        var command = connection.CreateCommand();
-        command.CommandText = @"CREATE TABLE IF NOT EXISTS Henkilot (id INTEGER PRIMARY KEY, Nimi TEXT, Puhelin INTEGER);";
-        command.ExecuteNonQuery();
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = @"CREATE TABLE IF NOT EXISTS Henkilot (id INTEGER PRIMARY KEY, Nimi TEXT, Puhelin INTEGER);";
+            command.ExecuteNonQuery();
 
-        command.CommandText = @"CREATE TABLE IF NOT EXISTS Lemmikit (id INTEGER PRIMARY KEY, Nimi TEXT, Rotu TEXT, OmistajaID INTEGER, FOREIGN KEY(OmistajaID) REFERENCES Henkilot(id));";
-        command.ExecuteNonQuery();
+            command.CommandText = @"CREATE TABLE IF NOT EXISTS Lemmikit (id INTEGER PRIMARY KEY, Nimi TEXT, Rotu TEXT, OmistajaID INTEGER;";
+            command.ExecuteNonQuery();
+        }
     }
 
     public void LisaaHenkilo(string nimi, int puhelin)
     {
-        var connection = new SqliteConnection(_connectionString);
-        connection.Open();
-        var command = connection.CreateCommand();
-        command.CommandText = @"INSERT INTO Henkilot (Nimi, Puhelin) VALUES ($nimi, $puhelin);";
-        command.Parameters.AddWithValue("$nimi", nimi);
-        command.Parameters.AddWithValue("$puhelin", puhelin);
-        command.ExecuteNonQuery();
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+            var commandForCheck = connection.CreateCommand();
+            commandForCheck.CommandText = @"SELECT id FROM Henkilot WHERE Nimi = $nimi AND Puhelin = $puhelin;";
+            commandForCheck.Parameters.AddWithValue("$nimi", nimi);
+            commandForCheck.Parameters.AddWithValue("$puhelin", puhelin);
+            object? id = commandForCheck.ExecuteScalar();
+        }
     }
 
     public void LisaaLemmikki(string nimi, string rotu, int omistajaID)
